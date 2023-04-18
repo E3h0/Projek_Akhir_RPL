5cd4543be2ba31f01768202604f2af66e568e806
E3h0e5 <fathan.x.a.m@gmail.com>
2023-04-18 16:31:35 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<!-- live reload stuff -->
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TESSSSS</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Help</a></li>
+				<li><a href="#">About us</a></li>
+				<li><a href="login.php">Log in</a></li>
+			</ul>
+		</nav>
+	</header>
+	<h1>Overcoming Loneliness: Practical Steps to Feeling Better</h1>
+	<h2>measure a person's level of loneliness</h2>
+
+</body>
+</html>
