73cc335e3002607397f4defc38d27e4fba95262a
E3h0e5 <fathan.x.a.m@gmail.com>
2023-05-23 14:02:38 +0700

    tambah page psikolog

---
+++ psikolog.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Psikolog</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <h2>Psikolog</h2>
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="https://bicarakan-prod-bucket.s3.amazonaws.com/media/uploads/counselors/Psikolog_jakarta_Fajrin_Trisnaramawati.webp" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Fajrin Trisnaramawati M.Psi.</h4>
+      <p class="card-text">The quick brown fox jumps over the lazy dog</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+  <br>
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="https://o-cdn-cas.sirclocdn.com/parenting/images/Nidya_Dwika_Puteri_M.Psi_Psikolog.width-800.format-webp.webp" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Nidya Dwika Puteri M.Psi.</h4>
+      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+  <br>
+  <div class="card" style="width:400px">
+    <img class="card-img-top" src="https://o-cdn-cas.sirclocdn.com/parenting/images/Edward_Gani_M._Psi._Psikolog_-_bi.width-800.format-webp.webp" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Edward Gani, M. Psi</h4>
+      <p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
