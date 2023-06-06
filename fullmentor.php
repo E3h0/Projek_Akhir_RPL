6280985b09e0ec854f0b10eab15e0cfc8a7d8ef9
Muhammad Ariiq Arrafi <117450224+ariiqarrafi@users.noreply.github.com>
2023-06-06 22:13:12 +0700

    Add files via upload

---
+++ fullmentor.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mentor Page</title>
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;700&family=Raleway:wght@200;400;700&family=Rambla:wght@700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- My Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- My Styles -->
+    <link rel="stylesheet" href="style/style12.css" />
+  </head>
+  <body>
+    <div class="container">
+        <div class="header">
+            <div class="header-title">
+                <h1>Lonelines Test</h1>
+            </div>
+            <div class="header-backto">
+                <a href="home.php">Back to homepage</a>
+            </div>
+        </div>
+        <div class="banner">
+            <div class="banner-header">
+                <div class="banner-header-title">
+                    <h1>Mentor/Psikolog</h1>
+                </div>
+                <div class="banner-header-img">
+                    <div class="img-left">
+                    </div>
+                    <div class="img-right">
+                    </div>
+                </div>
+            </div>
+            <div class="banner-desc">
+                <p>Mentor/Psikolog yang direkomendasikan merupakan profesional yang memiliki pengalaman luas dalam bidang konseling dan psikologi. Mereka telah terbukti membantu banyak individu dalam mengatasi masalah-masalah emosional, kesejahteraan mental, dan perasaan kesepian. Dengan pengetahuan dan keahlian mereka, mentor/psikolog ini mampu memberikan dukungan dan panduan yang efektif dalam proses konseling, serta menyediakan lingkungan yang aman dan terpercaya untuk berbagi dan mencari solusi. Rekomendasi mereka didasarkan pada kompetensi, reputasi, dan rekam jejak mereka dalam membantu individu dalam menghadapi tantangan kehidupan sehari-hari.<br><br></p>
+            </div>
+            <div class="banner-mentor">
+                <div class="banner-mentor-img-top">
+                    <div class="mentor-1">
+                        <div class="sesi-1">
+                            <p>513+ sesi</p>
+                        </div>
+                        <div class="blur-1">
+                            <div class="name-mentor1">
+                                <a href="amr.html"><p>Amr, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-2">
+                        <div class="sesi-2">
+                            <p>467+ sesi</p>
+                        </div>
+                        <div class="blur-2">
+                            <div class="name-mentor2">
+                                <a href="natasha.html"><p>Natasha, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-3">
+                        <div class="sesi-3">
+                            <p>456+ sesi</p>
+                        </div>
+                        <div class="blur-3">
+                            <div class="name-mentor3">
+                                <a href="danielle.html"><p>Danielle, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-4">
+                        <div class="sesi-4">
+                            <p>389+ sesi</p>
+                        </div>
+                        <div class="blur-4">
+                            <div class="name-mentor4">
+                                <a href="george.html"><p>George, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="banner-mentor-img-bottom">  
+                    <div class="mentor-5">
+                        <div class="sesi-5">
+                            <p>400+ sesi</p>
+                        </div>
+                        <div class="blur-5">
+                            <div class="name-mentor5">
+                                <a href="nathalie.html"><p>Nathalie, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-6">
+                        <div class="sesi-6">
+                            <p>402+ sesi</p>
+                        </div>
+                        <div class="blur-6">
+                            <div class="name-mentor6">
+                                <a href="lizzie.html"><p>Lizzie, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-7">
+                        <div class="sesi-7">
+                            <p>399+ sesi</p>
+                        </div>
+                        <div class="blur-7">
+                            <div class="name-mentor7">
+                                <a href="lia.html"><p>Lia, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mentor-8">
+                        <div class="sesi-8">
+                            <p>299+ sesi</p>
+                        </div>
+                        <div class="blur-8">
+                            <div class="name-mentor8">
+                                <a href="tanlee.html"><p>Tan Lee, S.Psi</p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+      feather.replace();
+    </script>
+  </body>
+</html>
