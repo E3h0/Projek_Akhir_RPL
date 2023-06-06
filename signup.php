9bac8cd981c592ea500b2a0b45731dd89378327b
Muhammad Ariiq Arrafi <117450224+ariiqarrafi@users.noreply.github.com>
2023-06-06 18:30:33 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,99 +1,134 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Sign Up</title>
-</head>
-<body>
-	<a href="index.php"><p style="text-align: right;">Back to homepage</p></a>
-	<script>
-		function togglePassword(id) {
- 	 		var x = document.getElementById(id);
-			var button = x.nextElementSibling;
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign Up</title>
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;700&family=Raleway:wght@200;400;700&family=Rambla:wght@700&display=swap"
+      rel="stylesheet"
+    />
 
-		 	if (x.type === "password") {
-		    	x.type = "text";
-		    	button.textContent = "Hide";
-		  	} 
-		  	else {
-		    	x.type = "password";
-		    	button.textContent = "Show";
-		  	}
-		}
+    <!-- My Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
 
-		function checkPassword() {
-			var password = document.getElementById("pwd").value;
-			var confirmPassword = document.getElementById("c_pwd").value;
-			if (password != confirmPassword) {
-			    alert("Password and confirm password do not match!");
-			    document.getElementById("Myform").reset();
-			} 
-			else {
-			    document.getElementById("Myform").submit();
-			}
-		}
-	</script>
+    <!-- My Styles -->
+    <link rel="stylesheet" href="style/style8.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <div class="header-title">
+          <h1>Lonelines Test</h1>
+        </div>
+        <div class="header-backto">
+          <a href="index.php">Back to homepage</a>
+        </div>
+      </div>
+      <div class="banner">
+        <div class="banner-welcome">
+          <h1>Welcome back!</h1>
+          <p>Already have Account? <a href="login.php">Log In</a></p>
+        </div>
+        <div class="banner-form">
+          <form id="MyForm" action="signup.php" method="post">
+            <div class="banner-form-left">
+              <label>Full Name</label>
+              <input type="text" name="fullname" placeholder="Enter your fullname" required/>
+              <label>Username</label>
+              <input type="text" name="username" placeholder="Create username" required/>
+              <label>Date of Birth</label>
+              <input type="date" name="dob" placeholder="" required/>
+              <label>Password</label>
+              <input type="password" id="pwd" name="password" minlength="8" placeholder="Create password"/>
+			      </div>
+			      <div class="banner-form-right">
+              <label>Email</label>
+              <input type="email" name="email" placeholder="Enter email" required/>
+              <label>Gender</label>
+              <div class="gender">
+                <div class="male">
+                  <input type="radio" id="Male" name="gender" value="Male" required>
+                  <label for="Male">Male</label>
+                </div>
+                <div class="female">
+                  <input type="radio" id="Female" name="gender" value="Female" required>
+                  <label for="Female">Female</label>
+                </div>
+              </div>
+              <label>Phone Number</label>
+              <input type="text" name="phone" placeholder="Enter phone number"/>
+              <label>Confirm password</label>
+              <input type="password" id="c_pwd" name="c_pwd" placeholder="Enter password"/>
+            </div>
+			      <div class="banner-form-button">
+              <button type="submit" name="Submit" onclick="checkPassword()">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="footer">
+        <div class="footer-line"></div>
+      </div>
+    </div>
 
-	<form id="Myform" action="signup.php" method="post">
-		<label>Full Name</label><br>
-		<input type="text" name="fullname" placeholder="Enter your fullname" 
-		required><br>
-		<label>Username</label><br>
-		<input type="text" name="username" placeholder="Create username" 
-		required><br>
+    <?php
+    include "config.php";
 
-		<label>Date of Birth</label><br>
-		<input type="date" name="dob" placeholder="" required><br>
+    if(isset($_POST['Submit'])){
 
-		<label>Email</label><br>
-		<input type="email" name="email" placeholder="Enter email" required><br>
+      $fname = $_POST['fullname'];
+      $uname = $_POST['username'];
+      $dob = $_POST['dob'];
+      $email = $_POST['email'];
+      $gen = $_POST['gender'];
+      $phone = $_POST['phone'];
+      $pwd = $_POST['password'];
 
-		<label>Gender</label><br>
-		<input type="radio" id="Male" name="gender" value="Male" required>
-		<label for="Male">Male</label>
-		<input type="radio" id="Female" name="gender" value="Female">
-		<label for="Female">Female</label><br>
+      $sign_up = "INSERT INTO users(`username`, `fullname`, `gender`, `phone`, `dob`, `email`, `password`) VALUES ('$uname', '$fname', '$gen', '$phone', '$dob', '$email', '$pwd')";
+      $do = mysqli_query($connect, $sign_up);
 
-		<label>Phone Number</label><br>
-		<input type="text" name="phone" placeholder="Enter phone number"><br>
+      if($do){
+        echo '<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>';
+        echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+        echo "<script>
+                $(document).ready(function() {
+                    Swal.fire({
+                        title: 'Berhasil Membuat Akun',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    }).then(function() {
+                        window.location.href = 'login.php';
+                    });
+                });
+            </script>";
+        exit();
+      } else{
+          echo '<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>';
+          echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+          echo "<script>
+                  $(document).ready(function() {
+                      Swal.fire({
+                          title: 'Gagal Membuat Akun',
+                          text: 'Terdapat kesalahan dalam pembuatan akun.',
+                          icon: 'error',
+                          confirmButtonText: 'OK'
+                      });
+                  });
+              </script>";
+      }
 
-		<label>Password</label><br>
-		<input type="password" id="pwd" name="password" minlength="8" placeholder="Create password">
-		<button type="button" onclick="togglePassword('pwd')">Show</button><br>
-		<label>Confirm Password</label><br>
-		<input type="password" id="c_pwd" name="c_pwd" placeholder="Enter password">
-		<button type="button" onclick="togglePassword('c_pwd')">Show</button><br>
-
-		<button type="submit" name="Submit" onclick="checkPassword()">Submit</button>
-	</form>
-
-	<?php
-		include "config.php";
-
-		if(isset($_POST['Submit'])){
-
-			$fname = $_POST['fullname'];
-			$uname = $_POST['username'];
-			$dob = $_POST['dob'];
-			$email = $_POST['email'];
-			$gen = $_POST['gender'];
-			$phone = $_POST['phone'];
-			$pwd = $_POST['password'];
-		
-
-			$sign_up = "insert into users(
-			`username`,`fullname`,`gender`,`phone`,`dob`,`email`,`password`)
-			values('$uname','$fname','$gen','$phone','$dob','$email','$pwd')";
-			$do = mysqli_query($connect, $sign_up);
-			if($do){
-				echo "<script>alert('Sign Up Succesfully!!!')</script>";
-				header("Location: index.php");
-			}
-			else{
-				echo("Fail");
-			}
-		}
-	?>
-</body>
+      mysqli_close($connect);
+    }
+?>
+    <script>
+      feather.replace();
+    </script>
+  </body>
 </html>
