6a99a10538da5965c076066973735ab99ea925c1
Muhammad Ariiq Arrafi <117450224+ariiqarrafi@users.noreply.github.com>
2023-06-06 18:28:49 +0700

    Update pre_test.php

--- pre_test.php
+++ pre_test.php
@@ -1,25 +1,57 @@
 <?php 
 	$id = $_GET['id'];
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Loneliness Level Test</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;700&family=Raleway:wght@200;400;700&family=Rambla:wght@700&display=swap"
+    rel="stylesheet"/>
+  
+    <!-- My Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+  
+    <!-- My Styles -->
+    <link rel="stylesheet" href="style/style5.css">
 </head>
 <body>
-	<h2>Loneliness Level Test</h2>
-	<a type="hidden" href="home.php"><p style="text-align: right;">Back to home</p></a>
-	<p>Meningkatnya rasa kesepian dapat berdampak buruk pada kesehatan mental Anda, oleh karena itu disarankan untuk mengevaluasi tingkat kesepian Anda sebagai tindakan awal untuk mengurangi risikonya.</p>
-
-	<h2>Panduan Loneliness Level Test!</h2>
-	<ol>
-		<li>Isilah dengan jujur</li>
-		<li>Tidak ada jawaban benar atau salah</li>
-		<li>Cari tempat yang nyaman supaya bisa fokus</li>
-		<li>Hasil tes bisa didapatkan setelah mengisi semua pertanyaan dengan lengkap</li>
-	</ol>
-	<a href="tes1.php?id=<?php echo $id?>"><button>Mulai test</button></a>
+    <div class="container">
+        <div class="header">
+            <div class="header-title">
+                <h1>Loneliness Level Test</h1>
+            </div>
+        </div>
+        <div class="banner">
+            <div class="banner-desc">
+                <p>Meningkatnya rasa kesepian dapat berdampak buruk pada kesehatan mental Anda, oleh karena itu disarankan untuk mengevaluasi tingkat kesepian Anda sebagai tindakan awal untuk mengurangi risikonya.</p>
+            </div>
+            <div class="banner-img">
+                <img src="style/Checklist.jpg" alt="">
+            </div>
+            <div class="banner-panduan">
+                <h2>Panduan Loneliness Level Test</h1>
+                <ul>
+                    <li>1. Isilah dengan jujur</li>
+                    <li>2. Tidak ada jawaban yang benar ataupun salah</li>
+                    <li>3. Cari tempat yang nyaman supaya fokus</li>
+                    <li>4. Hasil tes bisa didapatkan setelah mengisi semua pertanyaan dengan lengkap.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footer-button">
+                <a href="tes1.php?id=<?php echo $id?>">Mulai Tes</a>
+            </div>
+            <div class="footer-line"></div>
+        </div>
+    </div>
 </body>
 </html>
