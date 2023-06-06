6167805d9842ef73900c7825d2c13c7124148399
Muhammad Ariiq Arrafi <117450224+ariiqarrafi@users.noreply.github.com>
2023-06-06 18:23:16 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,6 +3,30 @@
 session_start();
 session_destroy();
 
-header("Location: index.php");
-
+echo '<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>';
+echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+echo "<script>
+    $(document).ready(function() {
+        Swal.fire({
+            title: 'Log Out?',
+            text: 'Anda akan keluar dari halaman ini. Apakah Anda yakin?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Anda berhasil keluar!',
+                    icon: 'success'
+                }).then(function() {
+                    window.location.href = 'index.php';
+                });
+            } else {
+            }
+        });
+    });
+</script>";
 ?>
